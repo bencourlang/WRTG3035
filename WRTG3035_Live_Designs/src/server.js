8da14c44fb40c8eebf5994c5c6d3d204eefd7ef7
bencourlang <41306238+bencourlang@users.noreply.github.com>
2021-09-10 13:24:59 -0600

    Add files via upload

---
+++ WRTG3035_Live_Designs/src/server.js
@@ -0,0 +1,79 @@
+/***********************
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+  Pg-Promise   - A database tool to help use connect to our PostgreSQL database
+***********************/
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//Create Database Connection
+var pgp = require('pg-promise')();
+
+/**********************
+  Database Connection information
+  host: This defines the ip address of the server hosting our database.
+		We'll be using `db` as this is the name of the postgres container in our
+		docker-compose.yml file. Docker will translate this into the actual ip of the
+		container for us (i.e. can't be access via the Internet).
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab,
+		we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database. We set this in the
+		docker-compose.yml for now, usually that'd be in a seperate file so you're not pushing your credentials to GitHub :).
+**********************/
+const dev_dbConfig = {
+	host: 'db',
+	port: 5432,
+	database: process.env.POSTGRES_DB,
+	user:  process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD
+};
+
+/** If we're running in production mode (on heroku), the we use DATABASE_URL
+ * to connect to Heroku Postgres.
+ */
+const isProduction = process.env.NODE_ENV === 'production';
+const dbConfig = isProduction ? process.env.DATABASE_URL : dev_dbConfig;
+
+// Heroku Postgres patch for v10
+// fixes: https://github.com/vitaly-t/pg-promise/issues/711
+if (isProduction) {
+  pgp.pg.defaults.ssl = {rejectUnauthorized: false};
+}
+
+const db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+
+
+app.get('/', function(req, res) {
+	res.render('pages/design1',{
+		my_title:"Design 1"
+	});
+});
+
+app.get('/design2', function(req, res) {
+	res.render('pages/design2',{
+		my_title:"Design 2"
+	});
+});
+
+app.get('/design3', function(req, res) {
+	res.render('pages/design3',{
+		my_title:"Design 3"
+	});
+});
+
+
+app.listen(3000);
+console.log('3000 is the magic port');
